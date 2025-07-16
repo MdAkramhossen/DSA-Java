5ca3e5101e04f0811e23e55cdc0135b527707a82
Akram hossen <mdakramhossen613@gmail.com>
2025-07-16 08:13:02 +0600

    Add Binary Search Tree implementation

---
+++ BinarySearchTree/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+     /*   BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(2);
+        myBST.insert(1);
+        myBST.insert(3);
+        System.out.println("Root: " + myBST.root.value);
+        System.out.println("\nRoot->Left: " + myBST.root.left.value);
+        System.out.println("\nRoot->Right: " + myBST.root.right.value);*/
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.contains(27));
+
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.contains(17));
+
+    }
+}
